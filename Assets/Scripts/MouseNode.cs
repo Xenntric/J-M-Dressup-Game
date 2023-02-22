861d62dc69da76547c4919a9f658446fafa2eff6
Xenntric <matt.j.bowes@gmail.com>
2023-02-22 01:17:41 +0000

    Mouse Control Redux
    So after getting mouse control working in the previous version, there was a frustrating error where if you overlap two objects and grab them, you'll grab both. This has been fixed by *finally* getting all items to communicate with the mouse node, it turns out I did this already in my dissertation and completely forgot how. Now you can click on objects, drag them around, and if you hide an object behind another youll still be connected to the one you last dragged. Not sure this is the best solution and I may add a proper Zindex targeting system but for now it works well enough.

--- Assets/Scripts/MouseNode.cs
+++ Assets/Scripts/MouseNode.cs
@@ -1,61 +1,71 @@
 using Godot;
 using System;
-
+using System.Collections.Generic;
 public partial class MouseNode : Node
 {
 	public bool clicked {get;set;}
 	private Node NodeUnderMouse{get;set;}
+	public List<Node2D> Items_nodes{get;set;}
+	public List<Area2D> Items_areas{get;set;}
+
 	public bool Hovering;
-	// public override void _Ready()
-	// {
-	// 	this.AddUserSignal("mouse_entered");
-	// 	//GD.Print("Signal list:" + this.GetSignalList());
+	public override void _Ready()
+	{
+		Items_nodes = new List<Node2D>();
+		Items_areas = new List<Area2D>();
+
+	}
+	public override void _Input(InputEvent @event)
+	{
+		base._Input(@event);
 		
-	// }
-	// public override void _Input(InputEvent @event)
-	// {
-	// 	base._Input(@event);
-		
-	// 	if (@event.IsActionPressed("Grab"))
-	// 	{
-	// 		clicked = true;
+		if (@event.IsActionPressed("Grab"))
+		{
+			clicked = true;
 
-	// 		GD.Print("Grabbed");
-	// 	}
-	// 	else if (@event.IsActionReleased("Grab"))
-	// 	{
-	// 		GD.Print("Released");
+			GD.Print("Grabbed");
+		}
+		else if (@event.IsActionReleased("Grab"))
+		{
+			GD.Print("Released");
 
-	// 		clicked = false;
-	// 	}
-	// }
-	// public override void _Process(double delta)
-	// {
-		
-	// }
+			clicked = false;
+		}
+	}
+	public override void _Process(double delta)
+	{
+		if(Items_nodes.Count >= 1 && clicked)
+		{
+			
+			//GD.Print(Items_nodes[0].Name);
+			if(clicked)
+			{
+					var folderParent = Items_nodes[0].GetParent<Object>() as Node2D;
+					//var originalPos = folderParent.Position + Items_nodes[0].Position;
 
-	// public void Mousey()
-	// {
-	// 	GD.Print("friend your Father");
+					Items_nodes[0].Position = GetViewport().GetMousePosition() - folderParent.Position;
+			} else {Items_nodes[0].Position = Items_nodes[0].Position;}
+		}
+			
+		//GD.Print("Current Nodes: " + Items_nodes.Capacity);
+	}
 
-	// }
+	public void addNode(Node2D node2D, Area2D area2D)
+	{
+		Items_nodes.Add(node2D);
+	}
+	public void identifyNode(string nodePath)
+	{
+		GD.Print(nodePath);
+	}
 
-	// public void Bingus(string text)
-	// {
-	// 	GD.Print("bingoid " + text);
-	// }
-
-
-	// private void _on_area_2d_mouse_entered(NodePath nodePath)
-	// {
-
-	//     //Node2D node = GetNode<>(nodePath);
-		
-	//     Hovering = true;
-	// }
-	// private void _on_area_2d_mouse_exited()
-	// {
-	//     Hovering = false;
-	// }
+	private void _on_area_2d_mouse_entered(NodePath nodePath)
+	{		
+	    Hovering = true;
+	}
+	private void _on_area_2d_mouse_exited()
+	{
+	    Hovering = false;
+	}
 	
 }
--- Assets/Scripts/SignalScript.cs
+++ Assets/Scripts/SignalScript.cs
@@ -3,61 +3,73 @@ using System;
 
 public partial class SignalScript : Area2D
 {
-    public bool clicked {get;set;}
+	public bool clicked {get;set;}
 	public bool Hovering;
-    Callable mouseSignalOn;
-    Callable mouseSignalOff;
-    Node2D thisParent;
-    Node2D folderParent;
+	Callable mouseSignalOn;
+	Callable mouseSignalOff;
 
-    Vector2 originalPos;
+	Callable pingNode;
 
-    public override void _Ready()
+	Node2D thisParent;
+	Node2D folderParent;
+
+	Vector2 originalPos;
+
+	MouseNode MN;
+	public override void _Ready()
 	{
-        thisParent = this.GetParent<Node2D>();
-        folderParent = thisParent.GetParent<Object>() as Node2D;
-        originalPos = folderParent.Position + thisParent.Position;
-
-        GD.Print("This Parent: "+ thisParent.Name +"\n" + "This Folder: " + folderParent.Name);
-        GD.Print("Signal Script Connecting");
-        mouseSignalOn = new Callable(this, "mouse_on");
-        mouseSignalOff = new Callable(this, "mouse_off");
-        this.Connect("mouse_entered",   mouseSignalOn);
-        this.Connect("mouse_exited",    mouseSignalOff);
-    }
-    public override void _Input(InputEvent @event)
-	{
-		base._Input(@event);
+		MN = GetNode<MouseNode>(new NodePath("/root/DressUpScene/MouseControlNode"));
 		
-		if (@event.IsActionPressed("Grab") && Hovering)
-		{
-			clicked = true;
-			GD.Print("Grabbed: " + this.Name + " @ Z index: " + this.ZIndex);
-		}
-		else if (@event.IsActionReleased("Grab") && Hovering)
-		{
-			GD.Print("Released");
+        thisParent = this.GetParent<Node2D>();
+		folderParent = thisParent.GetParent<Object>() as Node2D;
+		originalPos = folderParent.Position + thisParent.Position;
 
-			clicked = false;
-		}
+		// GD.Print("This Parent: "+ thisParent.Name +"\n" + "This Folder: " + folderParent.Name);
+		// GD.Print("Signal Script Connecting");
+	    mouseSignalOn = new Callable(this, "mouse_on");
+	    mouseSignalOff = new Callable(this, "mouse_off");
+		// pingNode = new Callable(GetNode<Node>(new NodePath("/root/DressUpScene/MouseControlNode")), "identifyNode");
+
+		this.Connect("mouse_entered",   mouseSignalOn);
+		this.Connect("mouse_exited",    mouseSignalOff);
+		// this.Connect("mouse_entered",   pingNode);
+        // EmitSignal("mouse_entered", this.GetPath());
+
+		//MN.addNode(thisParent, null);
+	}
+	public override void _Input(InputEvent @event)
+	{
+		// base._Input(@event);
+		
+		// if (@event.IsActionPressed("Grab") && Hovering)
+		// {
+		// 	clicked = true;
+		// 	GD.Print("Grabbed: " + this.Name + " @ Z index: " + this.ZIndex);
+		// }
+		// else if (@event.IsActionReleased("Grab") && Hovering)
+		// {
+		// 	GD.Print("Released");
+
+		// 	clicked = false;
+		// }
 	}
 
-    public void mouse_on()
-    {
-        Hovering = true;
-    }
-    public void mouse_off()
-    {
-        Hovering = false;
-    }
-    public override void _Process(double delta)
+	public void mouse_on()
 	{
-		if(clicked)
-        {
-            //GD.Print("mouse pos: " + GetViewport().GetMousePosition());
-            thisParent.Position = GetViewport().GetMousePosition() - (originalPos);
-        }
-        else{this.Position = this.Position;}
+        MN.Items_nodes.Add(thisParent);
+	}
+	public void mouse_off()
+	{
+        MN.Items_nodes.Remove(thisParent);
+	}
+	public override void _Process(double delta)
+	{
+		// if(clicked)
+		// {
+		// 	//GD.Print("mouse pos: " + GetViewport().GetMousePosition());
+		// 	thisParent.Position = GetViewport().GetMousePosition() - (originalPos);
+		// }
+		// else{this.Position = this.Position;}
 	}
 
 }
