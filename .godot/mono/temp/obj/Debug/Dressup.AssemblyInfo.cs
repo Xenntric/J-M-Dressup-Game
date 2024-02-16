ab6303ec38d2af67b097c2276c73db9307ca858d
matt <matt.j.bowes@gmail.com>
2024-02-16 18:49:05 +0000

    Clothes menu refactor
    slightly cleaner setup, next is to get the page menus working

--- .godot/mono/temp/obj/Debug/Dressup.AssemblyInfo.cs
+++ .godot/mono/temp/obj/Debug/Dressup.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Dressup")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8972f7ff1ec7392430ec42483104be77c0d8e656")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2b4ce2e02064dbbd684b49a74e3d60e9e7a67525")]
 [assembly: System.Reflection.AssemblyProductAttribute("Dressup")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Dressup")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Assets/Scripts/MainGame/UI/PopulateItems.cs
+++ Assets/Scripts/MainGame/UI/PopulateItems.cs
@@ -6,133 +6,87 @@ using System.Linq;
 using System.Net.NetworkInformation;
 using System.Reflection;
 
-public partial class PopulateItems : Control
+public partial class PopulateItems : TextureRect
 {
-	//private TextureRect ItemContainer;
-	private TextureRect MasterContainer;
-	private Container FolderContainer;
-	private HBoxContainer Rows;
-	private VBoxContainer Columns;
-	private List<ItemList> ItemLists;
-	private List<Texture> clothes;
+    //private TextureRect ItemContainer;
+    private TextureRect MasterContainer;
+    private Container FolderContainer;
+    private HBoxContainer Rows;
+    private VBoxContainer Columns;
+    private List<ItemList> ItemLists;
+    private List<Texture> clothes;
 
-	private Entity entity;
-	[Export] public int AmountOfRows = 3;
-	//private int itemScanned;
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
-	{
-		//ItemLists = new List<ItemList>();
-		MasterContainer = GetNode<TextureRect>(GetChild<TextureRect>(0).GetPath());
-		//Sulong objId = FolderContainer.GetInstanceId();
-		//SFolderContainer.SetScript(ResourceLoader.Load("Assets/Scripts/MainGame/UI/ItemSelect.cs"));
-		//SFolderContainer = InstanceFromId(objId) as Container;
+    private Entity entity;
+    [Export] public int AmountOfRows = 3;
 
-		
-		createFolders();
-		//populateFolders();
-	}
+    private int foldersScanned;
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+        ProcessPriority = 0;
+        MasterContainer = GetNode<TextureRect>(GetPath());
+        createFolders();
+    }
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
-	}
+    public void createFolders()
+    {
+        foreach (var folder in Directory.GetDirectories(@"Assets/Art/Clothes/"))
+        {
+            List<Texture2D> itemInFolder = new();
+            foreach (string item in Directory.GetFiles(folder, "*.png"))
+            {
+                itemInFolder.Add((Texture2D)GD.Load(item));
+                GD.Print(item);
+            }
 
-	public void createFolders()
-	{
-		foreach (var folder in Directory.GetDirectories(@"Assets/Art/Clothes/"))
-		{
-			List<Texture2D> itemInFolder = new List<Texture2D>();
-			foreach (string item in Directory.GetFiles(folder, "*.png"))
-			{
-				itemInFolder.Add((Texture2D) GD.Load(item));
-				GD.Print(item);
-			}
+            int itemScanned = 0;
 
-			int itemScanned = 0;
+            FolderContainer = new Container
+            {
+                Name = Path.GetFileNameWithoutExtension(folder),
+                Size = MasterContainer.Size
+            };
+            
+			foldersScanned++;
 
-			FolderContainer = new Container();
-			FolderContainer.Name = Path.GetFileNameWithoutExtension(folder);
-			FolderContainer.Size = MasterContainer.Size;
-			MasterContainer.AddChild(FolderContainer);
-			Columns = new VBoxContainer();
-			Columns.SetPosition(new Vector2(25,25));
-			FolderContainer.AddChild(Columns);
+            if (foldersScanned > 1)
+            {
+                FolderContainer.Visible = false;
+            }
 
-			for (int i = 0; i < AmountOfRows; i++)
-			{
-				Rows = new HBoxContainer();
-				Columns.AddChild(Rows);
-				
-				for (int j = 0; j < 3; j++)
-				{
-					//GD.Print(itemScanned + 1);
-					//GD.Print(itemInFolder.Count());
-					if(itemScanned < itemInFolder.Count())
-					{
-						var itemButton = new TextureButton();
-						itemButton.TextureNormal = itemInFolder.ElementAt(itemScanned);
-						itemButton.StretchMode = TextureButton.StretchModeEnum.KeepAspect;
-						itemButton.IgnoreTextureSize = true;
-						itemButton.CustomMinimumSize = new Vector2(70,70);
-						Rows.AddChild(itemButton);
-						itemScanned++;
-					}
-				}
-			}
-			
-			// GD.Print(folder);
-			// var newFolder = new ItemList();
-			// 	newFolder.Name =  Path.GetFileNameWithoutExtension(folder);
-			// 	newFolder.FixedIconSize = new Vector2I(75,90);
-			// 	newFolder.SameColumnWidth = true;
-			// 	newFolder.FixedColumnWidth = 75;
-			// 	newFolder.MaxColumns = 3;
-			// 	newFolder.Size = ItemContainer.Size;
-			// 	newFolder.AllowSearch = false;
-			// 	newFolder.AllowReselect = true;
+            MasterContainer.AddChild(FolderContainer);
 
-			// 	//newFolder.Visible = false;
+            Columns = new VBoxContainer();
+            Columns.SetPosition(new Vector2(25, 25));
+            FolderContainer.AddChild(Columns);
 
-			foreach (string item in Directory.GetFiles(folder, "*.png"))
-			{
-				//var clothesContainer = new Container();
-				//clothesContainer.Size = new Vector2(100,100);			
+            for (int i = 0; i < AmountOfRows; i++)
+            {
+                Rows = new HBoxContainer();
+                Columns.AddChild(Rows);
 
-				//var texture = (Texture2D)GD.Load(item);
-				//var newItem = new TextureRect();
-				//newItem.Texture = texture;
-				//newItem.Name = item;
-				//newItem.ExpandMode = TextureRect.ExpandModeEnum.KeepSize; 
-				//newItem.Size = (new Vector2(100,100));
-				//newItem.StretchMode = 0;
-				
-				//clothesContainer.AddChild(newItem);
-				////newItem.SetSize(new Vector2(100,100), false);
-				//ItemContainer.AddChild(clothesContainer);
-				//newFolder.AddIconItem(texture,true);
-			
-			}
-			// // TabContainer.AddChild(newTab);
+                for (int j = 0; j < 3; j++)
+                {
+                    if (itemScanned < itemInFolder.Count)
+                    {
+                        var itemButton = new TextureButton
+                        {
+                            TextureNormal = itemInFolder.ElementAt(itemScanned),
+                            StretchMode = TextureButton.StretchModeEnum.KeepAspect,
+                            IgnoreTextureSize = true,
+                            CustomMinimumSize = new Vector2(70, 70)
+                        };
+                        Rows.AddChild(itemButton);
+                        itemScanned++;
+                    }
+                }
+            }
+        }
+    }
 
-			// // }
-			// // // var newTab = new ItemList();
-			// // // newTab.Name =  Path.GetFileNameWithoutExtension(folder);
-			//  ItemContainer.AddChild(newFolder);
-			//  ulong objId = newFolder.GetInstanceId();
-			//  newFolder.SetScript(ResourceLoader.Load("Assets/Scripts/MainGame/UI/ItemSelect.cs"));
-			//  newFolder = InstanceFromId(objId) as ItemList;
-			//  newFolder._Ready();
+    private void populateFolders()
+    {
 
-			// //var item = new TextureRect();
-			// //item.Name = Path.GetFileNameWithoutExtension(folder);
-
-			
-		}
-	}
-
-	private void populateFolders()
-	{
-
-	}
+    }
 }
+
