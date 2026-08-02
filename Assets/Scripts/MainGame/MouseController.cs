94acf1560ca6985de96ad7b897a3b775970a01c3
Matthew Bowes <matt.j.bowes@gmail.com>
2026-08-02 18:16:54 +0100

    Implement item rotation
    Player can now rotate items when holding down the right mouse button
    
    TODO: Implement shader to better communicate rotation degrees

--- Assets/Scripts/MainGame/MouseController.cs
+++ Assets/Scripts/MainGame/MouseController.cs
@@ -1,15 +1,17 @@
 using Godot;
 using System;
-using System.ComponentModel;
-using System.Diagnostics;
 using System.Linq;
-
+using System.ComponentModel;
 namespace Dressup
 {
     public partial class MouseController
     {
         private readonly Globals globals;
         private Vector2 mousePos = new(0,0);
+        private bool rotating;
+        private LiveItem rotatingItem;
+        private RichTextLabel rotationLabel;
+        private Vector2 rotationLabelOffset = new(10,5);
         public MouseController(Globals globals)
         {
             this.globals = globals;
@@ -17,43 +19,51 @@ namespace Dressup
 
         public void Input(InputEvent @event)
         {
-            if (@event.IsActionReleased("Grab", true))
-            {
-                Released();
-            }
-            if (globals.itemStack?.Count <= 0) { return; }
-            if (@event.IsActionPressed("Grab", true))
-            {
-               Pressed();
-            }
-
-            if (@event is InputEventMouseMotion eventMouseMotion)
-            {
-                Moving(eventMouseMotion);
-            }
+            HandleRotation(@event);
+            if (rotating) { return; }
+            HandleMovement(@event);           
         }
-
+        private void HandleMovement(InputEvent @event)
+        {
+            if (@event.IsActionReleased("Grab", true)) { Released(); }
+            if (globals.itemStack?.Count <= 0) { return; }
+            if (@event.IsActionPressed("Grab", true)) { Pressed(); }
+            if (@event is InputEventMouseMotion eventMouseMotion) { Moving(eventMouseMotion); }
+        }
         private void Pressed()
         {
+            GD.Print(TakeStackNames());
             AttachAndMove(FindExpectedItem());
         }
 
         private void Released()
         {
-
             if (globals.trash.Inside && globals.GrabbedItem != null)
             {
-                globals.trash.Flush();
+                globals.trash.Flush(globals.GrabbedItem);
             }
             else if (globals.magnetTarget != null)
             {
-                globals.magnetTarget.translateToMagnet();
+                globals.magnetTarget.TranslateToMagnet();
+                GD.Print($"{globals.GrabbedItem} && {globals.trash.CheckConflictingItems(globals.GrabbedItem)}");
+                globals.ItemsInPlace.Add(globals.GrabbedItem);
+                var conflictingItems = globals.trash.CheckConflictingItems(globals.GrabbedItem);
+                if (conflictingItems.Count > 0)
+                {
+                    foreach (var item in globals.trash.CheckConflictingItems(globals.GrabbedItem))
+                    {
+                        globals.trash.Flush(item);
+                    }
+                }
+
             }
+            GD.Print(TakeStackNames());
             globals.GrabbedItem = null;
         }
 
         private void Moving(InputEventMouseMotion eventMouseMotion)
         {
+            
             CleanUpItems();
             if (globals.GrabbedItem == null) { return; }
             mousePos = eventMouseMotion.Position;
@@ -93,7 +103,7 @@ namespace Dressup
                         Vector2 mp = globals.GetViewport().GetMousePosition();
                         Vector2 itemOrigin = item.GlobalPosition - new Vector2(((item.Texture.GetWidth() * globals.getCharacterScale.X)), (item.Texture.GetHeight() * globals.getCharacterScale.Y));
                         Vector2 itemBounds = new(itemOrigin.X + (item.Texture.GetWidth() * globals.getCharacterScale.X)*2, itemOrigin.X + (item.Texture.GetHeight() * globals.getCharacterScale.Y)*2);
-                        if ( mp >= itemOrigin && mp <= itemBounds)
+                        if (mp >= itemOrigin && mp <= itemBounds)
                         {
                             return;
                         }
@@ -105,6 +115,70 @@ namespace Dressup
                     throw new WarningException("Exiting Loop as ItemStack was Changed");
                 }
             }
+        }
+
+        private string TakeStackNames()
+        {
+            string names = "";
+            foreach (var item in globals.itemStack)
+            {
+                names += $"{item.Name} ";
+            }
+            return names;
+        }
+        
+        private void HandleRotation(InputEvent @event)
+        {
+            if (@event.IsActionPressed("Rotate", true) && this.globals.GrabbedItem != null && rotating == false)
+            {
+                rotating = true;
+                rotatingItem = globals.GrabbedItem.GetNode<LiveItem>(globals.GrabbedItem.GetPathTo(globals.GrabbedItem, true));
+                CreateNewLabel();
+            }
+
+            if (@event.IsActionReleased("Rotate", true) && rotating == true)
+            {
+                rotating = false;
+                var localTween = globals.GetTree().CreateTween();
+                rotatingItem.PosOffset = new(0,0);
+                localTween.TweenProperty(rotatingItem, "global_position", globals.GetViewport().GetMousePosition(), .05f)
+                            .SetTrans(Tween.TransitionType.Expo)
+                            .SetEase(Tween.EaseType.Out);
+                globals.RemoveChild(rotationLabel);
+                rotatingItem = null;
+            }
+
+            if (@event is InputEventMouseMotion && rotating)
+            {
+                RotateItem();
+            }
+        }
+
+        private void CreateNewLabel()
+        {
+            rotationLabel ??= new RichTextLabel
+            {
+                CustomMinimumSize = new(75, 0),
+                FitContent = true,
+                ZIndex = 100
+            };
+            globals.AddChild(rotationLabel);
+            rotationLabel.GlobalPosition = globals.GetViewport().GetMousePosition() + rotationLabelOffset;
+        }
+        private void RotateItem()
+        {
+            Vector2 itemPos = rotatingItem.GlobalPosition;
+            Vector2 mp = globals.GetViewport().GetMousePosition();
+            float angle = mp.AngleToPoint(itemPos);
+            rotatingItem.GlobalRotation = angle;
+            rotatingItem.PosOffset = globals.GetViewport().GetMousePosition() - rotatingItem.GlobalPosition;
+            rotationLabel.GlobalPosition = globals.GetViewport().GetMousePosition() + rotationLabelOffset;
+            string text = angle < 0 ? Mathf.RoundToInt(360 + Mathf.RadToDeg(angle)).ToString(): Mathf.RoundToInt(Mathf.RadToDeg(angle)).ToString();
+            text += '°';
+            rotationLabel.Text = text;
+            // DrawableTexture2D drawableTexture2D = new(); need to draw a line to the point of rotation so its a little easier to tell
+            // DrawLine(new Vector2(1.5f, 1.0f), new Vector2(1.5f, 4.0f), Colors.Green, 1.0f); // or it should be a shader.
+            // drawableTexture2D.draw
         }
     }
 }
